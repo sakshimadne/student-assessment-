cbe70d11459f6935be44a134f3a2f654de15d945
sakshimadne <sakshimadne12345@gmail.com>
2026-03-18 02:18:06 +0530

    full frontend backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,33 @@
 import AppRoutes from "./components/routes/AppRoutes";
-
+import { Toaster } from "react-hot-toast";
 
 function App() {
-  return <AppRoutes />;
+  return (
+    <>
+      <AppRoutes />
+
+      {/* TOASTER (GLOBAL) */}
+      <Toaster
+        position="top-right"
+        reverseOrder={false}
+        toastOptions={{
+          duration: 3000,
+          style: {
+            borderRadius: "12px",
+            background: "#111827",
+            color: "#fff",
+            padding: "12px 16px",
+          },
+          success: {
+            iconTheme: {
+              primary: "#6366f1",
+              secondary: "#fff",
+            },
+          },
+        }}
+      />
+    </>
+  );
 }
 
 export default App;
