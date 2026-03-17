0f48ad9e72b2a4b8966d51875947b5b3cbaeda65
sakshimadne <sakshimadne12345@gmail.com>
2026-03-17 22:01:57 +0530

    result controller

--- Backend/controllers/resultController.js
+++ Backend/controllers/resultController.js
@@ -1,23 +1,24 @@
 const Result = require("../models/Result")
 
-// Get logged-in user's result
 exports.getMyResult = async (req, res) => {
   try {
 
-    const result = await Result.findOne({ user: req.user })
+    const userId = req.user.id;
+
+    const result = await Result.findOne({ user: userId });
 
     if (!result) {
       return res.status(404).json({
         message: "No result found"
-      })
+      });
     }
 
-    res.json(result)
+    res.json(result);
 
   } catch (error) {
-    res.status(500).json({ error: error.message })
+    res.status(500).json({ error: error.message });
   }
-}
+};
 
 
 // Admin: Get all results
